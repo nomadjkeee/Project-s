e6c57b249a520e7308f76560630138ef4162b638
Nomadjkee_Local <nomad9965@gmail.com>
2020-04-06 09:43:48 +0300

    Add base views

---
+++ Vidly/Vidly/Vidly/Models/Movies.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Movies
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public short Year { get; set; }
+        public string Type { get; set; }
+    }
+}
