7b47b429384b4b16361165e04d62ce21fb4b0566
Nomadjkeee <cherukha.v@gmail.com>
2020-04-26 11:58:55 +0300

    AddMoviesController

---
+++ dev/dev/Vidly/Vidly/Vidly/App_Start/AutoMapperProfile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Vidly.Models;
+
+namespace Vidly.App_Start
+{
+    public class AutoMapperProfile:Profile
+    {
+        public AutoMapperProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<MemberShip, MemberShipDto>();
+
+            Mapper.CreateMap<MemberShipDto, MemberShip>()
+                .ForMember(t => t.Id, opt => opt.Ignore());
+
+            Mapper.CreateMap<CustomerDto, Customer>()
+                .ForMember(t => t.Id, opt => opt.Ignore());
+
+
+        }
+    }
+}
---
+++ dev/dev/Vidly/Vidly/Vidly/Models/CustomerDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Birthhday { get; set; }
+        public string Email { get; set; }
+        public MemberShipDto MemberShipDto { get; set; }
+        public int? MemberShipId { get; set; }
+
+    }
+    public class MemberShipDto
+    {
+        public int Id { get; set; }
+        public string MemberShipType { get; set; }
+    }
+}
